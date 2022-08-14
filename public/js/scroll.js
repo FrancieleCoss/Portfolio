9f82eff794b8553f09116073466b2380dbbc811d
FrancieleCoss <francielecosssima@gmail.com>
2022-08-14 13:16:53 -0300

    novo commit

---
+++ public/js/scroll.js
@@ -0,0 +1,10 @@
+window.sr = ScrollReveal({ reset:true});
+ScrollReveal({ distance: '15px' });
+sr.reveal('.desenho', {duration : 3000, origin: 'right'})
+sr.reveal('.descricao', {duration : 3000, origin: 'left'})
+sr.reveal('.linkedin', {duration : 3000,  origin: 'left'})
+sr.reveal('.desenho-bg', {duration : 3000,  origin: 'left'})
+sr.reveal('.sobre-mim', {duration : 3000, origin:'right'})
+sr.reveal('.github-link', {duration : 3000, origin:'bottom'})
+sr.reveal('.conteiner-skills', {duration : 3000, origin:'left'})
+sr.reveal('.conteiner-descricao', {duration : 3000, origin:'right'})
